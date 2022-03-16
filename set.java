13175909f41447369b497fce01ee3101a04b9656
YosepFernando <99975386+YosepFernando@users.noreply.github.com>
2022-03-16 22:21:26 +0800

    Create set.java

---
+++ set.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Set<String> set = new HashSet<String>();
+
+    set.add("Denpasar");
+    set.add("Karangasem");
+    set.add("Klungkung");
+    set.add("Ubud");
+
+    System.out.println(set);
+
+    }
+}
