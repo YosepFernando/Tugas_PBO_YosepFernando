03a034eaabe968907aaac5cfb7496a7e936977bc
YosepFernando <99975386+YosepFernando@users.noreply.github.com>
2022-03-16 23:01:45 +0800

    Update set.java

--- set.java
+++ set.java
@@ -4,14 +4,18 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	Set<String> set = new HashSet<String>();
+        Set<String> set = new HashSet<String>();
 
-    set.add("Denpasar");
-    set.add("Karangasem");
-    set.add("Klungkung");
-    set.add("Ubud");
+        set.add("Denpasar");
+        set.add("Karangasem");
+        set.add("Klungkung");
+        set.add("Ubud");
 
-    System.out.println(set);
+        System.out.println(set);
+
+        set.remove("Ubud");
+
+        System.out.println(set);
 
     }
 }
